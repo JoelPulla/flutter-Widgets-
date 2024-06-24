8228c0de96f7081fc2224ad8d8f8eb58d0a011ad
Joel Pulla <joel.pulla@outlook.com>
2024-06-24 15:34:43 -0500

    final animated container ui

--- lib/presentation/screen/animated/animated_screen.dart
+++ lib/presentation/screen/animated/animated_screen.dart
@@ -1,13 +1,66 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   // definimos nombre de la ruta
   static const name = 'animated_screen';
 
   const AnimatedScreen({super.key});
 
   @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 45;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+  // Void
+  void changeShape() {
+    final random = Random();
+
+    width = random.nextInt(300) + 50;
+    height = random.nextInt(400) + 20;
+    borderRadius = random.nextInt(100) + 10;
+    color = Color.fromRGBO(
+      random.nextInt(255),
+      random.nextInt(255),
+      random.nextInt(255),
+      1,
+    );
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text('Animated Container '),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          //animated
+          curve: Curves.ease,
+
+          // pra evitar si cnabiamos la aimaciones algun efecto me de un numero negativo y se rompa la app
+          height: height <= 0 ? 0 : width,
+          width: width <= 0 ? 0 : height,
+
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeShape,
+        child: const Icon(Icons.play_arrow),
+      ),
+    );
   }
 }
